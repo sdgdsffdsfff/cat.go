97d843d9fbad4365ca56ce0acb3a5ff6986514ff
phyxdown <wucc@Ctrip.com>
2015-06-16 16:47:46 +0800

    activate core apis

--- cat.go
+++ cat.go
@@ -3,6 +3,17 @@ package cat
 var cat_lock chan int = make(chan int, 1)
 var cat_initialized bool = false
 
+//A tool instance for CAT.
+//Use it to create Transaction, Event, Heartbeat, Trace...
+//Every Cat instance has 1 Tree instance.
+type Cat interface{
+	Tree
+}
+
+type cat struct {
+	Tree
+}
+
 //Cat_init_if initialize cat.go,
 //which must be down before any other operations, 
 //for which Instance called it automatically.
@@ -19,6 +30,9 @@ func Cat_init_if() {
 //As it's not recommended to apply thread local in go, 
 //apps with cat.go have to call Instance, 
 //keep and manage the instance returned properly.
-func Instance() interface{} {
-	return nil
+func Instance() Cat {
+	Cat_init_if()
+	return &cat{
+		NewTree(),
+	}
 }
