e502a12d427bf8438ee674d293d2d6c13d2bbba8
phyxdown <wucc@Ctrip.com>
2015-06-16 17:10:21 +0800

    sender receive transaction

--- sender.go
+++ sender.go
@@ -1,14 +1,20 @@
 package cat
 
 import "net"
+import "fmt"
 
-var Tchan chan Transaction = make(chan Transaction)
+var Tchan chan Transaction = make(chan Transaction, 256)
 
 //sender_run is internally used and only called by Cat_init_if.
 //sender_run receive Transaction from a specific channel, 
 //encode Transaction and start new goroutines to send Transaction to backend.
 //Basically, only 1 goroutine keeps the function.
 func sender_run() {
+	for {
+		t := <-Tchan
+		fmt.Println(t.GetName())
+		fmt.Println(t.GetType())
+	}
 }
 
 //sender_send is internally used and only called by sender_run
