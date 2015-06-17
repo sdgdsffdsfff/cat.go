1ce7f74d4c3f4db654402a522f631ecbccecb227
phyxdown <wucc@Ctrip.com>
2015-06-17 11:22:43 +0800

    rename Message to Meta, add Encodable & Message

---
+++ meta.go
@@ -0,0 +1,75 @@
+package cat
+
+import "time"
+import "bytes"
+import "fmt"
+
+type Meta interface{
+	SetStatus(Error)
+	Add(string, string)
+	GetType() string
+	GetName() string
+	GetStatus() string
+	GetTimestamp() time.Time
+	GetData() []byte
+}
+
+type meta struct {
+	m_type string
+	m_name string
+	m_status string
+	m_timestamp time.Time
+	m_data *bytes.Buffer
+}
+
+func NewMeta(t string, n string) Meta{
+	return &meta{
+		m_type: t,
+		m_name: n,
+		m_status: "unset",
+		m_timestamp: time.Now(),
+		m_data: nil,
+	}
+}
+
+func (m *meta) SetStatus(err Error) {
+	if err == nil {
+		m.m_status = "0"
+	} else {
+		m.m_status = fmt.Sprintf("%v", err)
+	}
+}
+
+func (m *meta) Add(key string, value string) {
+	if m.m_data == nil {
+		m.m_data = new(bytes.Buffer)
+		m.m_data.WriteString(key)
+		m.m_data.WriteString("=")
+		m.m_data.WriteString(value)
+	} else {
+		m.m_data.WriteString("&")
+		m.m_data.WriteString(key)
+		m.m_data.WriteString("=")
+		m.m_data.WriteString(value)
+	}
+}
+
+func (m *meta) GetType() string {
+	return m.m_type
+}
+
+func (m *meta) GetName() string {
+	return m.m_name
+}
+
+func (m *meta) GetStatus() string {
+	return m.m_status
+}
+
+func (m *meta) GetTimestamp() time.Time {
+	return m.m_timestamp
+}
+
+func (m *meta) GetData() []byte {
+	return m.m_data.Bytes()
+}
--- transaction.go
+++ transaction.go
@@ -1,6 +1,8 @@
 package cat
 
 import "time"
+import "bytes"
+import "strconv"
 
 type Transaction interface{
 	Message
@@ -9,7 +11,7 @@ type Transaction interface{
 }
 
 type transaction struct {
-	Message
+	Meta
 	f Function
 	start time.Time
 	end time.Time
@@ -19,7 +21,7 @@ type transaction struct {
 
 func NewTransaction(t string, n string, f Function) Transaction{
 	return &transaction{
-		NewMessage(t, n),
+		NewMeta(t, n),
 		f,
 		time.Now(),
 		time.Now(),
@@ -40,4 +42,52 @@ func (t *transaction) Complete() {
 	t.end = time.Now()
 	t.duration = time.Since(t.start)
 	Invoke(t.f, t)
+}
+
+func (t *transaction) Encode(buf *bytes.Buffer) Error{
+	if t.children == nil || len(t.children) == 0 {
+		buf.WriteString("A")
+		buf.WriteString(t.start.Format("2006-01-02 15:04:05.999"))
+		buf.WriteString(TAB)
+		buf.WriteString(t.GetType())
+		buf.WriteString(TAB)
+		buf.WriteString(t.GetName())
+		buf.WriteString(TAB)
+		buf.WriteString(t.GetStatus())
+		buf.WriteString(TAB)
+		buf.WriteString(strconv.FormatInt(int64(t.duration/1000), 10))
+		buf.WriteString("us")
+		buf.WriteString(TAB)
+		buf.Write(t.GetData())
+		buf.WriteString(TAB)
+		buf.WriteString(LF)
+	} else {
+		buf.WriteString("t")
+		buf.WriteString(t.start.Format("2006-01-02 15:04:05.999"))
+		buf.WriteString(TAB)
+		buf.WriteString(t.GetType())
+		buf.WriteString(TAB)
+		buf.WriteString(t.GetName())
+		buf.WriteString(TAB)
+		buf.WriteString(LF)
+		for _, child := range t.children {
+			child.Encode(buf)
+		}
+		buf.WriteString("T")
+		buf.WriteString(t.end.Format("2006-01-02 15:04:05.999"))
+		buf.WriteString(TAB)
+		buf.WriteString(t.GetType())
+		buf.WriteString(TAB)
+		buf.WriteString(t.GetName())
+		buf.WriteString(TAB)
+		buf.WriteString(t.GetStatus())
+		buf.WriteString(TAB)
+		buf.WriteString(strconv.FormatInt(int64(t.duration/1000), 10))
+		buf.WriteString("us")
+		buf.WriteString(TAB)
+		buf.Write(t.GetData())
+		buf.WriteString(TAB)
+		buf.WriteString(LF)
+	}
+	return recover()
 }
