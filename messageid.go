5c65854af70151e9987b8934377bf1d3cf90ea16
phyxdown <wucc@Ctrip.com>
2015-06-18 17:35:47 +0800

    better mechanism to generate messageid

--- messageid.go
+++ messageid.go
@@ -7,24 +7,38 @@ type MessageIdFactory interface {
 type message_id_factory struct {
 	index int
 	index_l chan int
+	next_ids chan int
 }
 
 func NewMessageIdFactory() MessageIdFactory {
 	return &message_id_factory{
 		0,
 		make(chan int, 1),
+		make(chan int, 1<<10),
 	}
 }
 
 var MESSAGE_ID_FACTORY MessageIdFactory = NewMessageIdFactory()
 
 func (f *message_id_factory) Next() MessageId {
-	next := NewMessageId()
+	select {
+	case nextid := <-f.next_ids:
+		next := NewMessageId()
+		next.SetIndex(nextid)
+		return next
+	default:
+		go f.generate()
+		return f.Next()
+	}
+}
+
+func (f *message_id_factory) generate() {
 	f.index_l <- 0
-	next.SetIndex(f.index)
-	f.index++
+	for i := 0; i< 1<<10; i++ {
+		f.next_ids <- f.index
+		f.index++
+	}
 	<-f.index_l
-	return next
 }
 
 type MessageId interface {
