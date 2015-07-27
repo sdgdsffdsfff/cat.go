e3b5cf19604c63de56945c9d6d38a2f7c447db98
phyxdown <1768649634@qq.com>
2015-07-27 14:13:38 +0800

    abandon message when error counted in accessing temp file

--- tempfile.go
+++ tempfile.go
@@ -6,26 +6,27 @@ import (
 	. "os"
 	"syscall"
 	"time"
+	"errors"
 )
 
-func cat_new_mids() (floor uint64, ceiling uint64, tsh uint64) {
+func cat_new_mids() (floor uint64, ceiling uint64, tsh uint64, err error) {
 	tsh = uint64(time.Now().Unix() / 3600)
 	file, err := OpenFile(TEMPFILE, O_CREATE|O_RDWR, 0664)
 	if err != nil {
-		return 0, 0, tsh
+		return 0, 0, tsh, errors.New("Unable to open temp file")
 	}
 	defer file.Close()
 	share := make([]byte, 16)
 	syscall.Flock(int(file.Fd()), syscall.LOCK_EX)
 	n, err := file.Read(share)
 	if err != nil && err != io.EOF {
-		return 0, 0, tsh
+		return 0, 0, tsh, errors.New("Unable to read temp file")
 	}
 	if n == 16 {
 		var f uint64 = binary.BigEndian.Uint64(share[:8])
 		var l uint64 = binary.BigEndian.Uint64(share[8:])
 		if tsh > l {
-			floor = 0
+			floor = 1
 			ceiling = 100000
 			buf := make([]byte, 8)
 			binary.BigEndian.PutUint64(buf, uint64(ceiling))
@@ -40,22 +41,22 @@ func cat_new_mids() (floor uint64, ceiling uint64, tsh uint64) {
 			file.WriteAt(buf, 0)
 		}
 	} else if n == 0 {
-		floor = 0
+		floor = 1
 		ceiling = 100000
 		buf := make([]byte, 8)
 		binary.BigEndian.PutUint64(buf, uint64(ceiling))
 		n, err = file.WriteAt(buf, 0)
 		if err != nil {
-			return 0, 0, tsh
+			return 0, 0, tsh, errors.New("Unable to write index to temp file")
 		}
 		binary.BigEndian.PutUint64(buf, uint64(tsh))
 		n, err = file.WriteAt(buf, 8)
 		if err != nil {
-			return 0, 0, tsh
+			return 0, 0, tsh, errors.New("Unable to write tsh to temp file")
 		}
 	} else {
-		return 0, 0, tsh
+		return 0, 0, tsh, errors.New("Temp file is herpahs corrupted")
 	}
 	syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
-	return floor, ceiling, tsh
+	return floor, ceiling, tsh, nil
 }
