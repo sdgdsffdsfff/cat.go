7e178996aef8de93fc781e0527c296cbbc45f3c0
phyxdown <1768649634@qq.com>
2015-08-06 17:38:32 +0800

    update heartbeat.go

--- example/heartbeat.go
+++ example/heartbeat.go
@@ -1,27 +1,37 @@
 package main
 
-import cat "../"
-import "runtime"
+import (
+	cat "../"
+	"runtime"
+	"time"
+)
 
 func main(){
+	cat.DOMAIN = "555555"
+	cat.CAT_HOST = cat.UAT
 	runtime.GOMAXPROCS(4)
-	mycat := cat.Instance()
-	func(){
-		tr := mycat.NewTransaction("System", "Status")
-		defer func(){
-			tr.AddData("dumpLocked", "false")
-			p := recover()
-			tr.SetStatus(p)
-			tr.Complete()
-		}()
-		h := mycat.NewHeartbeat("Heartbeat", "192.168.141.131")
-		h.Set("System", "Total", "0.9")
-		h.Set("System", "I/O Thread", "0.1")
-		h.SetStatus("0")
-		h.Complete()
-	}()
+	for {
+		second := time.Now().Second()
+		heartbeat()
+		println(time.Now().Format("2006-01-02 15:04:05.999"))
+		time.Sleep(time.Duration(90 - second)*1000000000)
+	}
 	chan1 := make(chan int)
 	chan1 <- 0
 }
 
-
+func heartbeat(){
+	CAT := cat.Instance()
+	tr := CAT.NewTransaction("System", "Status")
+	defer func(){
+		tr.AddData("dumpLocked", "false")
+		p := recover()
+		tr.SetStatus(p)
+		tr.Complete()
+	}()
+	h := CAT.NewHeartbeat("Heartbeat", "192.168.141.131")
+	h.Set("System", "Total", "0.9")
+	h.Set("System", "I/O Thread", "0.1")
+	h.SetStatus("0")
+	h.Complete()
+}
